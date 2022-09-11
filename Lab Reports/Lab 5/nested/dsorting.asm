283cdabade24dc37839bdfeb2242ecb35045a1d7
Saad Ahmad <saad.ahmad1024@gmail.com>
2022-09-11 14:56:01 +0500

    happy birthday

---
+++ Lab Reports/Lab 5/nested/dsorting.asm
@@ -0,0 +1,46 @@
+[org 0x0100]
+
+jmp start       
+
+data: dw   6, 4, 5, 2
+
+start: 
+    mov  cx, 4                          
+
+    outerloop: 
+        mov  bx, 0    
+        
+        innerloop: 
+            mov  ax, [data + bx]
+
+            cmp  ax, [data + bx + 2]  
+
+            ja  noswap                 
+                ; the swap potion
+                mov  dx, [data + bx + 2]
+                mov  [data + bx + 2], ax
+                mov  [data + bx], dx
+
+            noswap:
+            add  bx, 2
+            cmp  bx, 6
+        jne  innerloop
+
+    ; check outer loop termination 
+    sub cx, 1 
+    jnz outerloop 
+
+    ; exit system call 
+    mov  ax, 0x4c00
+    int  0x21 
+
+; result will be 2 4 5 6
+
+
+
+
+
+
+
+
+ 
